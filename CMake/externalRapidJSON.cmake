972acd195f14d7fe0dd8e9bd2ab4e5d5764c5c33
chenbo <chenbo.chen@alibaba-inc.com>
2021-03-11 21:30:21 +0800

    Add md5 for external package downloading
    Signed-off-by: Bob Chen <beef9999@qq.com>

--- CMake/externalRapidJSON.cmake
+++ CMake/externalRapidJSON.cmake
@@ -4,6 +4,7 @@ ExternalProject_Add(
   RapidJSON
   PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/external/RapidJSON
   URL https://github.com/Tencent/rapidjson/archive/v1.1.0.tar.gz
+  URL_MD5 badd12c511e081fec6c89c43a7027bce
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
