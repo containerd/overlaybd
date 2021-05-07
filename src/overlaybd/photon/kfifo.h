d2ca17464f48be27fd331d4ed289a325489c224e
alpc62 <void00@foxmail.com>
2021-05-07 16:54:16 +0800

    optimize spsc-queue, support POD, support rvalue

--- src/overlaybd/photon/kfifo.h
+++ src/overlaybd/photon/kfifo.h
@@ -20,31 +20,46 @@
  */
 
 // Source https://github.com/torvalds/linux/blob/v2.6.32/include/linux/kfifo.h
+// And    https://github.com/torvalds/linux/blob/v2.6.32/kernel/kfifo.c
 // Modified By wujiaxu <void00@foxmail.com>
 
 #if defined (__cplusplus)
 extern "C" {
 #endif
 
-#ifndef _OVERLAYBD_KFIFO_H
-#define _OVERLAYBD_KFIFO_H
+#ifndef _KFIFO_H_62
+#define _KFIFO_H_62
 
 #include <pthread.h>
+#include <memory.h>
+#include <stdlib.h>
 
 struct kfifo {
-    void *buffer;             /* the buffer holding the data */
-    unsigned int size;        /* the size of the allocated buffer */
-    unsigned int in;          /* data is added at offset (in % size) */
-    unsigned int out;         /* data is extracted from off. (out % size) */
-    pthread_spinlock_t *lock; /* protects concurrent modifications */
+    pthread_spinlock_t lock; /* protects concurrent modifications */
+    void *buffer;            /* the buffer holding the data */
+    unsigned int size;       /* the size of the allocated buffer */
+    unsigned int in;         /* data is added at offset (in % size) */
+    unsigned int out;        /* data is extracted from off. (out % size) */
 };
 
-extern struct kfifo *kfifo_alloc(unsigned int size);
-extern void kfifo_free(struct kfifo *fifo);
-extern unsigned int __kfifo_put(struct kfifo *fifo,
-                const void *buffer, unsigned int len);
-extern unsigned int __kfifo_get(struct kfifo *fifo,
-                void *buffer, unsigned int len);
+static struct kfifo *kfifo_alloc(unsigned int size);
+static void kfifo_free(struct kfifo *fifo);
+
+// lock-free
+static void __kfifo_reset(struct kfifo *fifo);
+static unsigned int __kfifo_len(const struct kfifo *fifo);
+static unsigned int __kfifo_get(struct kfifo *fifo,
+                                void *buffer, unsigned int len);
+static unsigned int __kfifo_put(struct kfifo *fifo,
+                                const void *buffer, unsigned int len);
+
+// thread-safety by spin-lock
+static void kfifo_reset(struct kfifo *fifo);
+static unsigned int kfifo_len(struct kfifo *fifo);
+static unsigned int kfifo_get(struct kfifo *fifo,
+                              void *buffer, unsigned int len);
+static unsigned int kfifo_put(struct kfifo *fifo,
+                              const void *buffer, unsigned int len);
 
 /**
  * __kfifo_reset - removes the entire FIFO contents, no locking version
@@ -61,11 +76,11 @@ static inline void __kfifo_reset(struct kfifo *fifo)
  */
 static inline void kfifo_reset(struct kfifo *fifo)
 {
-    pthread_spin_lock(fifo->lock);
+    pthread_spin_lock(&fifo->lock);
 
     __kfifo_reset(fifo);
 
-    pthread_spin_unlock(fifo->lock);
+    pthread_spin_unlock(&fifo->lock);
 }
 
 /**
@@ -83,11 +98,11 @@ static inline unsigned int kfifo_put(struct kfifo *fifo,
 {
     unsigned int ret;
 
-    pthread_spin_lock(fifo->lock);
+    pthread_spin_lock(&fifo->lock);
 
     ret = __kfifo_put(fifo, buffer, len);
 
-    pthread_spin_unlock(fifo->lock);
+    pthread_spin_unlock(&fifo->lock);
 
     return ret;
 }
@@ -106,7 +121,7 @@ static inline unsigned int kfifo_get(struct kfifo *fifo,
 {
     unsigned int ret;
 
-    pthread_spin_lock(fifo->lock);
+    pthread_spin_lock(&fifo->lock);
 
     ret = __kfifo_get(fifo, buffer, len);
 
@@ -117,7 +132,7 @@ static inline unsigned int kfifo_get(struct kfifo *fifo,
     if (fifo->in == fifo->out)
         fifo->in = fifo->out = 0;
 
-    pthread_spin_unlock(fifo->lock);
+    pthread_spin_unlock(&fifo->lock);
 
     return ret;
 }
@@ -139,13 +154,158 @@ static inline unsigned int kfifo_len(struct kfifo *fifo)
 {
     unsigned int ret;
 
-    pthread_spin_lock(fifo->lock);
+    pthread_spin_lock(&fifo->lock);
 
     ret = __kfifo_len(fifo);
 
-    pthread_spin_unlock(fifo->lock);
+    pthread_spin_unlock(&fifo->lock);
 
     return ret;
+}
+
+
+static inline unsigned int _min(unsigned int a, unsigned int b)
+{
+    return (a < b) ? a : b;
+}
+
+/**
+ * kfifo_alloc - allocates a new FIFO and its internal buffer
+ * @size: the size of the internal buffer to be allocated.
+ *
+ * size MUST be a power of 2
+ */
+static inline struct kfifo *kfifo_alloc(unsigned int size)
+{
+    void *buffer;
+    struct kfifo *fifo;
+
+    /* size must be a power of 2 */
+    if ((size < 2) || (size & (size - 1)))
+        return NULL;
+
+    buffer = malloc(size);
+    if (!buffer)
+        return NULL;
+
+    fifo = (struct kfifo *)malloc(sizeof(struct kfifo));
+    if (!fifo)
+        return NULL;
+
+    fifo->buffer = buffer;
+    fifo->size = size;
+    fifo->in = fifo->out = 0;
+
+    if (pthread_spin_init(&fifo->lock, PTHREAD_PROCESS_PRIVATE) != 0)
+    {
+        free(fifo);
+        return NULL;
+    }
+
+    return fifo;
+}
+
+/**
+ * kfifo_free - frees the FIFO
+ * @fifo: the fifo to be freed.
+ */
+static inline void kfifo_free(struct kfifo *fifo)
+{
+    pthread_spin_destroy(&fifo->lock);
+    free(fifo->buffer);
+    free(fifo);
+}
+
+/**
+ * __kfifo_put - puts some data into the FIFO, no locking version
+ * @fifo: the fifo to be used.
+ * @buffer: the data to be added.
+ * @len: the length of the data to be added.
+ *
+ * This function copies at most @len bytes from the @buffer into
+ * the FIFO depending on the free space, and returns the number of
+ * bytes copied.
+ *
+ * Note that with only one concurrent reader and one concurrent
+ * writer, you don't need extra locking to use these functions.
+ */
+static inline unsigned int __kfifo_put(struct kfifo *fifo,
+                                       const void *buffer,
+                                       unsigned int len)
+{
+    unsigned int l;
+
+    len = _min(len, fifo->size - fifo->in + fifo->out);
+
+    /*
+     * Ensure that we sample the fifo->out index -before- we
+     * start putting bytes into the kfifo.
+     */
+
+    asm volatile("mfence" ::: "memory");
+
+    /* first put the data starting from fifo->in to buffer end */
+    l = _min(len, fifo->size - (fifo->in & (fifo->size - 1)));
+    memcpy((char *)fifo->buffer + (fifo->in & (fifo->size - 1)), buffer, l);
+
+    /* then put the rest (if any) at the beginning of the buffer */
+    memcpy(fifo->buffer, (char *)buffer + l, len - l);
+
+    /*
+     * Ensure that we add the bytes to the kfifo -before-
+     * we update the fifo->in index.
+     */
+
+    asm volatile("sfence" ::: "memory");
+
+    fifo->in += len;
+
+    return len;
+}
+
+/**
+ * __kfifo_get - gets some data from the FIFO, no locking version
+ * @fifo: the fifo to be used.
+ * @buffer: where the data must be copied.
+ * @len: the size of the destination buffer.
+ *
+ * This function copies at most @len bytes from the FIFO into the
+ * @buffer and returns the number of copied bytes.
+ *
+ * Note that with only one concurrent reader and one concurrent
+ * writer, you don't need extra locking to use these functions.
+ */
+static inline unsigned int __kfifo_get(struct kfifo *fifo,
+                                       void *buffer, unsigned int len)
+{
+    unsigned int l;
+
+    len = _min(len, fifo->in - fifo->out);
+
+    /*
+     * Ensure that we sample the fifo->in index -before- we
+     * start removing bytes from the kfifo.
+     */
+
+    asm volatile("lfence" ::: "memory");
+
+    /* first get the data from fifo->out until the end of the buffer */
+    l = _min(len, fifo->size - (fifo->out & (fifo->size - 1)));
+    memcpy(buffer, (char *)fifo->buffer + (fifo->out & (fifo->size - 1)), l);
+
+    /* then get the rest (if any) from the beginning of the buffer */
+    memcpy((char *)buffer + l, fifo->buffer, len - l);
+
+    /*
+     * Ensure that we remove the bytes from the kfifo -before-
+     * we update the fifo->out index.
+     */
+
+    asm volatile("mfence" ::: "memory");
+
+    fifo->out += len;
+
+    return len;
 }
 
 #endif
@@ -153,4 +313,3 @@ static inline unsigned int kfifo_len(struct kfifo *fifo)
 #if defined (__cplusplus)
 }
 #endif
-
--- src/overlaybd/photon/queue.h
+++ src/overlaybd/photon/queue.h
@@ -1,106 +1,284 @@
 /*
-   Copyright The Overlaybd Authors
+  Copyright (c) 2021 wujiaxu <void00@foxmail.com>
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 #pragma once
+#include <pthread.h>
+#include <stdlib.h>
+#include <utility>
 #include "kfifo.h"
 
+template<typename T, unsigned int capacity, bool = std::is_pod<T>::value>
+class __spsc_queue;
+
+template<typename T, unsigned int capacity, bool = std::is_pod<T>::value>
+class __mpmc_queue;
+
 // replace boost/lockfree/spsc_queue.hpp
-template <typename T, uint32_t capacity>
+template <typename T, unsigned int capacity>
 class spsc_queue
 {
-protected:
-    static constexpr unsigned int ptrSize = sizeof(T*);
-
 public:
-    spsc_queue()
-    {
-        fifo_ = kfifo_alloc(capacity * ptrSize);
-        if (!fifo_)
-            abort();
-    }
-
-    ~spsc_queue()
-    {
-        kfifo_free(fifo_);
-    }
-
     bool push(const T& t)
     {
-        return __kfifo_put(fifo_, &t, ptrSize) > 0;
+        return queue_.push(t);
+    }
+
+    bool push(T&& t)
+    {
+        return queue_.push(std::move(t));
     }
 
     int pop(T* ret, int n)
     {
-        return __kfifo_get(fifo_, ret, ptrSize * n) / ptrSize;
+        return queue_.pop(ret, n);
     }
 
     int read_available() const
     {
-        return __kfifo_len(fifo_) / ptrSize;
-    }
-
-    int read_available()
-    {
-        return kfifo_len(fifo_) / ptrSize;
+        return queue_.read_available();
     }
 
 private:
-    struct kfifo* fifo_;
+    __spsc_queue<T, capacity> queue_;
 };
 
-// lock-free multiple-producer/multiple-consumer ringbuffer
-template <typename T, uint32_t capacity>
+// thread-safety multiple-producer/multiple-consumer circular-queue
+template <typename T, unsigned int capacity>
 class mpmc_queue
 {
+public:
+    bool push(const T& t)
+    {
+        return queue_.push(t);
+    }
+
+    bool push(T&& t)
+    {
+        return queue_.push(std::move(t));
+    }
+
+    int pop(T* ret, int n)
+    {
+        return queue_.pop(ret, n);
+    }
+
+    int read_available() const
+    {
+        return queue_.read_available();
+    }
+
+private:
+    __mpmc_queue<T, capacity> queue_;
+};
+
+//// inner template
+// not for user
+
+/* T is POD. */
+template<typename T, unsigned int capacity>
+class __spsc_queue<T, capacity, true>
+{
 protected:
-    static constexpr unsigned int ptrSize = sizeof(T*);
+    static constexpr unsigned int sizeT = sizeof(T);
 
 public:
-    mpmc_queue()
+    __spsc_queue()
     {
-        fifo_ = kfifo_alloc(capacity * ptrSize);
+        fifo_ = kfifo_alloc(capacity * sizeT);
         if (!fifo_)
             abort();
     }
 
-    ~mpmc_queue()
+    ~__spsc_queue()
     {
         kfifo_free(fifo_);
     }
 
     bool push(const T& t)
     {
-        return kfifo_put(fifo_, &t, ptrSize) > 0;
+        return __kfifo_put(fifo_, &t, sizeT) > 0;
+    }
+
+    bool push(T&& t)
+    {
+        return __kfifo_put(fifo_, &t, sizeT) > 0;
     }
 
     int pop(T* ret, int n)
     {
-        return kfifo_get(fifo_, ret, ptrSize * n) / ptrSize;
+        return __kfifo_get(fifo_, ret, sizeT * n) / sizeT;
     }
 
     int read_available() const
     {
-        return __kfifo_len(fifo_) / ptrSize;
-    }
-
-    int read_available()
-    {
-        return kfifo_len(fifo_) / ptrSize;
+        return __kfifo_len(fifo_) / sizeT;
     }
 
 private:
     struct kfifo* fifo_;
 };
 
+/* T is not POD. */
+template<typename T, unsigned int size>
+class __spsc_queue<T, size, false>
+{
+public:
+    bool push(const T& t)
+    {
+        if (size - in_ + out_ == 0)
+            return 0;
+
+        buffer_[(in_++) & (size - 1)] = t;
+        return 1;
+    }
+
+    bool push(T&& t)
+    {
+        if (size - in_ + out_ == 0)
+            return 0;
+
+        buffer_[(in_++) & (size - 1)] = std::move(t);
+        return 1;
+    }
+
+    int pop(T* ret, int n)
+    {
+        unsigned int l;
+        unsigned int len = n;
+
+        len = _min(len, in_ - out_);
+
+        asm volatile("lfence" ::: "memory");
+
+        l = _min(len, size - (out_ & (size - 1)));
+
+        for (unsigned int i = 0; i < l; i++)
+            ret[i] = std::move(buffer_[(out_ & (size - 1)) + i]);
+
+        for (unsigned int i = 0; i < len - l; i++)
+            ret[l + i] = std::move(buffer_[i]);
+
+        asm volatile("mfence" ::: "memory");
+
+        out_ += len;
+
+        return len;
+    }
+
+    int read_available() const
+    {
+        return in_ - out_;
+    }
+
+private:
+    T buffer_[size];
+    unsigned int in_ = 0;
+    unsigned int out_ = 0;
+};
+
+/* T is POD. */
+template<typename T, unsigned int capacity>
+class __mpmc_queue<T, capacity, true>
+{
+protected:
+    static constexpr unsigned int sizeT = sizeof(T);
+
+public:
+    __mpmc_queue()
+    {
+        fifo_ = kfifo_alloc(capacity * sizeT);
+        if (!fifo_)
+            abort();
+    }
+
+    ~__mpmc_queue()
+    {
+        kfifo_free(fifo_);
+    }
+
+    bool push(const T& t)
+    {
+        return kfifo_put(fifo_, &t, sizeT) > 0;
+    }
+
+    bool push(T&& t)
+    {
+        return kfifo_put(fifo_, &t, sizeT) > 0;
+    }
+
+    int pop(T* ret, int n)
+    {
+        return kfifo_get(fifo_, ret, sizeT * n) / sizeT;
+    }
+
+    int read_available() const
+    {
+        return __kfifo_len(fifo_) / sizeT;
+    }
+
+private:
+    struct kfifo* fifo_;
+};
+
+/* T is not POD. */
+template<typename T, unsigned int capacity>
+class __mpmc_queue<T, capacity, false>
+{
+public:
+    __mpmc_queue()
+    {
+        if (pthread_spin_init(&lock_, PTHREAD_PROCESS_PRIVATE) != 0)
+            abort();
+    }
+
+    ~__mpmc_queue()
+    {
+        pthread_spin_destroy(&lock_);
+    }
+
+    bool push(const T& t)
+    {
+        pthread_spin_lock(&lock_);
+        bool res = queue_.push(t);
+        pthread_spin_unlock(&lock_);
+        return res;
+    }
+
+    bool push(T&& t)
+    {
+        pthread_spin_lock(&lock_);
+        bool res = queue_.push(std::move(t));
+        pthread_spin_unlock(&lock_);
+        return res;
+    }
+
+    int pop(T* ret, int n)
+    {
+        pthread_spin_lock(&lock_);
+        int res = queue_.pop(ret, n);
+        pthread_spin_unlock(&lock_);
+        return res;
+    }
+
+    int read_available() const
+    {
+        // no need lock
+        return queue_.read_available();
+    }
+
+private:
+    pthread_spinlock_t lock_;
+    __spsc_queue<T, capacity> queue_;
+};
