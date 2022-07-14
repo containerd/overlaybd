84a350cb443477d5f415b8eb22ea4b45a46be3ea
Yifan Yuan <tuji.yyf@alibaba-inc.com>
2022-07-14 23:53:25 +0800

    Support open file in LSMTFile format rather than ZFile format
    Signed-off-by: Yifan Yuan <tuji.yyf@alibaba-inc.com>

--- CMake/Findphoton.cmake
+++ CMake/Findphoton.cmake
@@ -4,7 +4,7 @@ set(FETCHCONTENT_QUIET false)
 FetchContent_Declare(
   photon
   GIT_REPOSITORY https://github.com/alibaba/PhotonLibOS.git
-  GIT_TAG main
+  GIT_TAG 7997b43e3d7cf27b95d016cd047abb9ca8063339
 )
 
 if(BUILD_TESTING)
