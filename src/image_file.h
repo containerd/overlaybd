containerd/overlaybd

commit e96e6c23174cbb50683aef0834eb6a3c54e34555

add registry v2
Signed-off-by: liulanzheng <lanzheng.liulz@alibaba-inc.com>

src/image_file.h
@@ -51,15 +51,12 @@ public:
     }
 
     ~ImageFile() {
-        delete m_file;
-        delete m_prefetcher;
-    }
-
-    int close() override {
         m_status = -1;
         if (dl_thread_jh != nullptr)
             photon::thread_join(dl_thread_jh);
-        return m_file->close();
+        delete m_prefetcher;
+        m_file->close();
+        delete m_file;
     }
 
     int fstat(struct stat *buf) override {

.github/workflows/cmake.yml
@@ -52,6 +52,7 @@ jobs:
       shell: bash
       run: |
         sudo make install
+        sudo cp ${{github.workspace}}/src/example_config/overlaybd-registryv2.json /etc/overlaybd/overlaybd.json
         sudo systemctl enable /opt/overlaybd/overlaybd-tcmu.service
         sudo systemctl start overlaybd-tcmu
         sudo systemctl status overlaybd-tcmu

CMake/Findphoton.cmake
@@ -4,7 +4,7 @@ set(FETCHCONTENT_QUIET false)
 FetchContent_Declare(
   photon
   GIT_REPOSITORY https://github.com/alibaba/PhotonLibOS.git
-  GIT_TAG v0.4.1
+  GIT_TAG v0.5.1
 )
 
 if(BUILD_TESTING)

CMake/Findrapidjson.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
   rapidjson
   GIT_REPOSITORY https://github.com/Tencent/rapidjson.git
-  GIT_TAG master
+  GIT_TAG 80b6d1c83402a5785c486603c5611923159d0894
 )
 FetchContent_GetProperties(rapidjson)
 if (NOT rapidjson_POPULATED)

src/config.h
@@ -88,6 +88,7 @@ struct GlobalConfig : public ConfigUtils::Config {
     APPCFG_PARA(enableAudit, bool, true);
     APPCFG_PARA(p2pConfig, P2PConfig);
     APPCFG_PARA(auditPath, std::string, "/var/log/overlaybd-audit.log");
+    APPCFG_PARA(registryFsVersion, std::string, "v1");
 };
 
 struct AuthConfig : public ConfigUtils::Config {
@@ -101,6 +102,6 @@ struct ImageAuthResponse : public ConfigUtils::Config {
     APPCFG_PARA(traceId, std::string, "");
 	APPCFG_PARA(success, bool, false);
 	APPCFG_PARA(data, AuthConfig);
-}; 
+};
 
 } // namespace ImageConfigNS

src/image_service.cpp
@@ -276,9 +276,12 @@ int ImageService::init() {
             }
         }
 
-        LOG_INFO("create registryfs with cafile:`", cafile);
+        LOG_INFO("create registryfs with cafile:`, version:`", cafile, global_conf.registryFsVersion());
+        auto registryfs_creator = new_registryfs_v1;
+        if (global_conf.registryFsVersion() == "v2")
+            registryfs_creator = new_registryfs_v2;
 
-        auto registry_fs = new_registryfs_with_credential_callback(
+        auto registry_fs = registryfs_creator(
             {this, &ImageService::reload_auth}, cafile, 30UL * 1000000);
         if (registry_fs == nullptr) {
             LOG_ERROR_RETURN(0, -1, "create registryfs failed.");

src/main.cpp
@@ -327,7 +327,6 @@ static int close_cnt = 0;
 static void dev_close(struct tcmu_device *dev) {
     obd_dev *odev = (obd_dev *)tcmu_dev_get_private(dev);
     delete odev->loop;
-    odev->file->close();
     delete odev->file;
     delete odev;
     close_cnt++;

src/overlaybd/registryfs/registryfs.h
@@ -28,8 +28,12 @@ public:
 using PasswordCB = Delegate<std::pair<std::string, std::string>, const char *>;
 
 extern "C" {
-photon::fs::IFileSystem *new_registryfs_with_credential_callback(PasswordCB callback,
-                                                     const char *caFile = nullptr,
-                                                     uint64_t timeout = -1);
+photon::fs::IFileSystem *new_registryfs_v1(PasswordCB callback,
+                                           const char *caFile = nullptr,
+                                           uint64_t timeout = -1);
+
+photon::fs::IFileSystem *new_registryfs_v2(PasswordCB callback,
+                                           const char *caFile = nullptr,
+                                           uint64_t timeout = -1);
 }
 

commit 90a7ef5777ce57018707844ef616a103dc9ba149

fix imagefile destruct bug was introduced in e96e6c2
Signed-off-by: liulanzheng <lanzheng.liulz@alibaba-inc.com>

src/image_file.h
@@ -55,8 +55,10 @@ public:
         if (dl_thread_jh != nullptr)
             photon::thread_join(dl_thread_jh);
         delete m_prefetcher;
-        m_file->close();
-        delete m_file;
+        if (m_file) {
+            m_file->close();
+            delete m_file;
+        }
     }
 
     int fstat(struct stat *buf) override {