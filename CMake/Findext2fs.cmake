742b79ad6421bdec197c283397e08ad342d0268c
yuchen.cc <yuchen.cc@alibaba-inc.com>
2023-04-06 18:50:39 +0800

    workflow: support multi-distro & multi-arch build
    Signed-off-by: yuchen.cc <yuchen.cc@alibaba-inc.com>

--- CMake/Findext2fs.cmake
+++ CMake/Findext2fs.cmake
@@ -1,21 +1,21 @@
 include(FetchContent)
 set(FETCHCONTENT_QUIET false)
 
-if (${ARCH} STREQUAL x86_64)
-    set(LIB_EXT2FS_URL "https://github.com/data-accelerator/e2fsprogs/releases/download/latest/libext2fs.tar.gz")
-elseif (${ARCH} STREQUAL aarch64)
-    set(LIB_EXT2FS_URL "https://github.com/data-accelerator/e2fsprogs/releases/download/latest/libext2fs.aarch64.tar.gz")
-endif ()
-
 FetchContent_Declare(
-  ext2fs
-  URL ${LIB_EXT2FS_URL}
+  e2fsprogs
+  GIT_REPOSITORY https://github.com/data-accelerator/e2fsprogs.git
+  GIT_TAG b4d19a0ec5cb535f13009d910833c62aa70d69a5
 )
 
-FetchContent_GetProperties(ext2fs)
-if (NOT ext2fs_POPULATED)
-  FetchContent_Populate(ext2fs)
-endif()
+FetchContent_MakeAvailable(e2fsprogs)
 
-set(EXT2FS_INCLUDE_DIR ${ext2fs_SOURCE_DIR}/include)
-find_library(EXT2FS_LIBRARY ext2fs HINTS ${ext2fs_SOURCE_DIR}/lib/)
+set(LIBEXT2FS_INSTALL_DIR ${e2fsprogs_SOURCE_DIR}/build/libext2fs)
+add_custom_command(
+  OUTPUT ${LIBEXT2FS_INSTALL_DIR}/lib
+  WORKING_DIRECTORY ${e2fsprogs_SOURCE_DIR}
+  COMMAND chmod 755 build.sh && ./build.sh
+)
+add_custom_target(libext2fs ALL DEPENDS ${LIBEXT2FS_INSTALL_DIR}/lib)
+
+set(EXT2FS_INCLUDE_DIR ${LIBEXT2FS_INSTALL_DIR}/include)
+set(EXT2FS_LIBRARY ${LIBEXT2FS_INSTALL_DIR}/lib/libext2fs.so)
