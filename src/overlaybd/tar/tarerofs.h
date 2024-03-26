25e47ef3e94389a23ff90e954331524edc9bf690
Hongzhen Luo <hongzhen.lhz@linux.alibaba.com>
2024-03-26 16:17:56 +0800

    Introduce tarerofs for TurboOCI-apply
    Currently erofs-utils is integrated into overlaybd by using `mkfs.erofs`
    executable file.  It's not optimial since raw data needs to be dumped
    first and output data needs to be write into overlaybd then.
    
    Later API integration may be a better form but it needs more work.
    
    Signed-off-by: Hongzhen Luo <hongzhen.lhz@linux.alibaba.com>

---
+++ src/overlaybd/tar/tarerofs.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <tar.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/param.h>
+#include <pwd.h>
+#include <grp.h>
+#include <string.h>
+#include <photon/fs/filesystem.h>
+#include <photon/fs/fiemap.h>
+#include <photon/common/string_view.h>
+#include <set>
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+
+class TarErofs {
+public:
+    TarErofs(photon::fs::IFile *file, photon::fs::IFile *target, uint64_t fs_blocksize = 4096,
+          photon::fs::IFile *bf = nullptr, bool meta_only = true)
+        : file(file), fout(target), fs_base_file(bf), meta_only(meta_only) {}
+
+    int extract_all();
+
+private:
+    photon::fs::IFile *file = nullptr;     // source
+    photon::fs::IFile *fout = nullptr; // target
+    photon::fs::IFile *fs_base_file = nullptr;
+    bool meta_only;
+    std::set<std::string> unpackedPaths;
+    std::list<std::pair<std::string, int>> dirs; // <path, utime>
+};
