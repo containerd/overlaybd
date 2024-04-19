3e6cfeab081ab1b60dd3373bce0258fa4b3ec796
liulanzheng <lanzheng.liulz@alibaba-inc.com>
2024-04-19 11:37:35 +0800

    update photon to v0.6.16
    Signed-off-by: liulanzheng <lanzheng.liulz@alibaba-inc.com>

--- CMake/Findphoton.cmake
+++ CMake/Findphoton.cmake
@@ -5,7 +5,7 @@ set(PHOTON_ENABLE_EXTFS ON)
 FetchContent_Declare(
   photon
   GIT_REPOSITORY https://github.com/alibaba/PhotonLibOS.git
-  GIT_TAG v0.6.15
+  GIT_TAG v0.6.16
 )
 
 if(BUILD_TESTING)
