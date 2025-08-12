75583f18f71c528a0abad1a68ff33f0b268fd28d
Alexander Dines <alex@runloop.ai>
2025-08-12 15:57:15 -0700

    cp dines

--- CMake/FindProtobuf.cmake
+++
@@ -1,4 +0,0 @@
-find_package(Protobuf REQUIRED)
-if(NOT Protobuf_FOUND)
-    message(FATAL_ERROR "Protobuf not found")
-endif()
--- CMake/FindgRPC.cmake
+++
@@ -1,4 +0,0 @@
-find_package(gRPC CONFIG REQUIRED)
-if(NOT gRPC_FOUND)
-    message(FATAL_ERROR "gRPC not found")
-endif()
